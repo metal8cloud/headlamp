7057deba0aa097b0545873ae1ac591a54eb4b8d0
yuli <stremovsky@gmail.com>
2024-05-29 16:03:37 +0300

    save

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 docker build -t headlamp .
-docker tag headlamp 192.168.64.3:32000/headlamp
-docker push 192.168.64.3:32000/headlamp
+#docker tag headlamp 192.168.64.3:32000/headlamp
+#docker push 192.168.64.3:32000/headlamp
+docker tag headlamp registry.metal8.cloud/headlamp
+docker push registry.metal8.cloud/headlamp 
 
 microk8s kubectl rollout restart -n kube-system deployment/headlamp
 echo "microk8s kubectl port-forward -n kube-system service/headlamp 3000:80"
